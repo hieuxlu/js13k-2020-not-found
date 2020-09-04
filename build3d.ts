cd80cf0cca93045edab208290a24b81aa1db4757
Hieu Do <xlu.untitled@gmail.com>
2020-09-04 08:15:44 +0700

    Support multiple z levels

--- build3d.ts
+++ build3d.ts
@@ -8,46 +8,47 @@ interface Shape3D {
   offset: number;
 }
 
-export const makeCube = (pos: Vec2, size: number) => {
+export const makeCube = (pos: Vec2, size: number, z: number = 0) => {
   const { x, y } = pos;
   const right = x + size;
   const top = y + size;
+  const height = z + size;
   const positions = [
     // Front face
-    right, top, size,
-    right, y, size,
-    x, y, size,
-    x, top, size,
+    right, top, height,
+    right, y, height,
+    x, y, height,
+    x, top, height,
 
     // Back face
-    right, top, 0.0,
-    right, y, 0.0,
-    x, y, 0.0,
-    x, top, 0.0,
+    right, top, z,
+    right, y, z,
+    x, y, z,
+    x, top, z,
 
     // Top face
-    x, top, 0.0,
-    right, top, 0.0,
-    right, top, size,
-    x, top, size,
+    x, top, z,
+    right, top, z,
+    right, top, height,
+    x, top, height,
 
     // Bottom face
-    x, y, 0.0,
-    x, y, size,
-    right, y, size,
-    right, y, 0.0,
+    x, y, z,
+    x, y, height,
+    right, y, height,
+    right, y, z,
 
     // Right face
-    right, y, 0.0,
-    right, y, 1.0,
-    right, top, 1.0,
-    right, top, 0.0,
+    right, y, z,
+    right, y, height,
+    right, top, height,
+    right, top, z,
 
     // Left face
-    x, y, 0.0,
-    x, top, 0.0,
-    x, top, size,
-    x, y, size,
+    x, y, z,
+    x, top, z,
+    x, top, height,
+    x, y, height,
   ]
 
   const textureCoordinates = [
@@ -109,7 +110,8 @@ export const makeLevel = (size: number) => {
   let vertexCount = 0;
   for (let x = 0; x < size; x++) {
     for (let y = 0; y < size; y++) {
-      const shape = makeCube(new Vec2(x - size / 2, y - size / 2), 1);
+      const z = Math.round(Math.random() * 10);
+      const shape = makeCube(new Vec2(x - size / 2, y - size / 2), 1, z);
       positions.push(...shape.positions);
       textureCoordinates.push(...shape.textureCoordinates);
       indices.push(...shape.indices.map(idx => idx + offset));
